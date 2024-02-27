6f602f32cdbf3c978a91ff8c4517f865e055349c
gaurav <singaurav181@gmail.com>
2024-02-27 23:34:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function gameEngine(){
         if(score>hiscoreval){
             hiscoreval = score;
             localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
-            hiscoreBox.innerHTML = "Heigh Score: "+ hiscoreval;
+            hiscoreBox.innerHTML = "High Score: "+ hiscoreval;
         }
         scoreBox.innerHTML = "Score: " + score;
         snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y})
